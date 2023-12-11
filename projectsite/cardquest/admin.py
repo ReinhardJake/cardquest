0115007e3c36b871d64e7e67246e77965147b3b5
reinhardjakesacay <reinhardjakesacayforwork@gmail.com>
2023-12-11 20:08:48 +0800

    editted pokemon

--- projectsite/cardquest/admin.py
+++ projectsite/cardquest/admin.py
@@ -1,9 +1,18 @@
 from django.contrib import admin
-from .models import PokemonCard
+from .models import PokemonCard, Trainer, Collection
 # Register your models here.
-admin.site.register(PokemonCard)
 
 @admin.register(PokemonCard)
 class PokemonAdmin(admin.ModelAdmin):
     list_display = ("name", "rarity")
     search_fields = ("name",)
+
+@admin.register(Trainer)
+class TrainerAdmin(admin.ModelAdmin):
+    list_display = ("name","birthdate","location","email")
+    search_fields = ("name",)
+
+@admin.register(Collection)
+class CollectionAdmin(admin.ModelAdmin):
+    list_display = ("card", "trainer", "collection_date")
+    search_fields = ("card", "trainer")
--- projectsite/cardquest/views.py
+++ projectsite/cardquest/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
-from cardquest.models import PokemonCard, Trainer
+from cardquest.models import PokemonCard, Trainer, Collection
 # Create your views here.
 
 class HomePageView(ListView):
@@ -11,8 +11,15 @@ class HomePageView(ListView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         return context
+    
 class TrainerList(ListView):
     model = Trainer
     context_object_name = 'trainer'
     template_name = 'trainer.html'
+    paginate_by = 15
+
+class CollectionList(ListView):
+    model = Collection
+    context_object_name = 'collection'
+    template_name = 'collection.html'
     paginate_by = 15
